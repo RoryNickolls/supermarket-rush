50a2ac14592b30678537463a58b22075135b9083
RoryNickolls <rorynickolls@gmail.com>
2020-03-28 10:07:34 +0000

    Add trails to trolley movement

--- Assets/Content/Scripts/Player.cs
+++ Assets/Content/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     [SerializeField]
@@ -17,27 +17,43 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private AudioClip crashClip;
 
-    private Rigidbody2D trolley;
+    [SerializeField]
+    private Trolley trolley;
+
+    private Rigidbody2D trolleyRb;
     private AudioSource audioSource;
+
+    private bool isDrifting = false;
 
     private void Start()
     {
-        trolley = GetComponent<Rigidbody2D>();
+        trolleyRb = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
         float stopping = 0.1f;
-        float speed = trolley.velocity.magnitude;
+        float speed = trolleyRb.velocity.magnitude;
         if(Input.GetAxis("Vertical") >= stopping && !audioSource.isPlaying)
         {
             audioSource.Play();
-  
         } 
         else if(Input.GetAxis("Vertical") < stopping) 
         {
             audioSource.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            trolley.StartDrifting();
+            isDrifting = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            trolley.StopDrifting();
+            isDrifting = false;
         }
     }
 
@@ -46,12 +62,14 @@ public class PlayerControl : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         float tempPushForce = movement.y < 0 ? brakeForce : pushForce;
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if(isDrifting)
         {
             tempPushForce *= 0.5f;
-        } 
-        trolley.AddForce(transform.up * movement.y * tempPushForce);
-        trolley.AddTorque(-movement.x * turnForce);
+        }
+
+        trolleyRb.AddForce(transform.up * movement.y * tempPushForce);
+        trolleyRb.AddTorque(-movement.x * turnForce);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
---
+++ Assets/Content/Scripts/Trolley.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trolley : MonoBehaviour
+{
+    private TrailRenderer[] trails;
+
+
+    private void Start()
+    {
+        trails = GetComponentsInChildren<TrailRenderer>();
+    }
+
+    public void StartDrifting()
+    {
+        foreach(TrailRenderer trail in trails)
+        {
+            trail.startColor = Color.yellow;
+            trail.endColor = new Color(1.0f, 0.7f, 0f);
+        }
+    }
+
+    public void StopDrifting()
+    {
+        foreach (TrailRenderer trail in trails)
+        {
+            trail.startColor = Color.white;
+            trail.endColor = new Color(1, 1, 1, 0);
+        }
+    }
+}
